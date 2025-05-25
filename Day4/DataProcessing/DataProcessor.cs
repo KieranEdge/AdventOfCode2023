18dbc1e5420ecebbf8a2936b31830a8180eab213
Kieran Edge <kesedge94@gmail.com>
2025-05-25 22:38:23 +0100

    Matches and scores set up

--- Day4/DataProcessing/DataProcessor.cs
+++ Day4/DataProcessing/DataProcessor.cs
@@ -9,12 +9,17 @@ namespace Day4.DataProcessing
     public class DataProcessor
     {
         private string _dataToProcess;
-        private string _referenceNumbersString;
-        private string _winningNumbersString;
+        private int[] _referenceNumbersInts;
+        private int[] _winningNumbersInts;
+        private int _winningNumbers = 0;
+        private int _totalPoints = 0;
+
         public DataProcessor(string dataToProcess)
         {
             _dataToProcess = dataToProcess;
             StringSplitter();
+            ProcessReferenceNumbers();
+            PointsTotal();
         }
 
         private void StringSplitter()
@@ -22,10 +27,52 @@ namespace Day4.DataProcessing
             // Expecting strings in the form of
             //Card   1: 30 48 49 69  1 86 94 68 12 85 | 86 57 89  8 81 85 82 68  1 22 90  2 74 12 30 45 69 92 62  4 94 48 47 64 49
             string combinedNumbersString = _dataToProcess.Split(':')[1];
-            _referenceNumbersString = combinedNumbersString.Split('|')[0];
-            _winningNumbersString = combinedNumbersString.Split('|')[1];
-            Console.WriteLine(_referenceNumbersString);
-            Console.WriteLine(_winningNumbersString);
+
+            _referenceNumbersInts = StringOfNumbersIntoArray(combinedNumbersString.Split('|')[0]);
+            _winningNumbersInts = StringOfNumbersIntoArray(combinedNumbersString.Split('|')[1]);
+        }
+
+        public int ReturnTotalScore()
+        {
+            return _totalPoints;
+        }
+
+        private int[] StringOfNumbersIntoArray(string NumbersStringToSplit)
+        {
+            string[] numbers = NumbersStringToSplit.Split(null);
+            List<int> numbersList = new List<int>();
+
+            foreach (string number in numbers)
+            {
+               if (number != "")
+               {
+                    numbersList.Add(int.Parse(number));
+               }
+            }
+            return numbersList.ToArray();
+        }
+
+        private void ReferenceNumberInWinningList(int referenceNumber)
+        {
+            if (_winningNumbersInts.Contains(referenceNumber))
+            {
+                _winningNumbers++;
+            }
+        }
+
+        private void ProcessReferenceNumbers()
+        {
+            foreach (int referenceNumber in _referenceNumbersInts)
+            {
+                ReferenceNumberInWinningList(referenceNumber);
+            }
+        }
+
+        private void PointsTotal()
+        {
+            Console.WriteLine($"Total number of winning number: {_winningNumbers}");
+            _totalPoints = (int)Math.Pow(2, _winningNumbers - 1);
+            Console.WriteLine($"Giving a total score of: {_totalPoints}");
         }
     }
 }
