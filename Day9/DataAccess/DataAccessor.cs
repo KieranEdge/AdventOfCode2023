f676984569f9f0c22eef66252933d3b65111b78b
Kieran Edge <kesedge94@gmail.com>
2025-06-02 23:16:14 +0100

    Data Access into list of arrays

---
+++ Day9/DataAccess/DataAccessor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day9.DataAccess
+{
+    public class DataAccessor
+    {
+        private string _filePath;
+        public List<int[]> _arrayOfSequences = new List<int[]>();
+
+        public DataAccessor(string filePath)
+        {
+            _filePath = filePath;
+            _arrayOfSequences = FileDataToArrayOfSequences();
+        }
+
+        private List<int[]> FileDataToArrayOfSequences()
+        {
+            List<int[]> sequences = File.ReadLines(_filePath)
+            .Select(line => line
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray())
+            .ToList();
+
+            return sequences;
+        }
+
+
+    }
+}
---
+++ Day9/Program.cs
@@ -0,0 +1,4 @@
+﻿using Day9.DataAccess;
+
+DataAccessor dataAccessor = new DataAccessor("C:\\Users\\Kieran Edge\\source\\repos\\AdventOfCode2023\\Day9\\Day9_data.txt");
+List<int[]> sequenceData = dataAccessor._arrayOfSequences;
