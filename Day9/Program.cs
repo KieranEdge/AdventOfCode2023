f356fda13ef1c5dfa028f0f6dabbdbad442653c1
Kieran Edge <kesedge94@gmail.com>
2025-06-02 23:49:13 +0100

    Part 1 Complete

--- Day9/Program.cs
+++ Day9/Program.cs
@@ -1,4 +1,20 @@
 ﻿using Day9.DataAccess;
+using Day9.SequenceAnalyser;
 
+// Accessing the data as a list of arrays
 DataAccessor dataAccessor = new DataAccessor("C:\\Users\\Kieran Edge\\source\\repos\\AdventOfCode2023\\Day9\\Day9_data.txt");
 List<int[]> sequenceData = dataAccessor._arrayOfSequences;
+
+// Analysing the sequences
+int[] extrapolatedValues = new int[sequenceData.Count];
+Console.WriteLine($"There are {sequenceData.Count} sequences to inspect");
+
+int nextValue = SequenceAnalyser.GetNextValueInSequence(sequenceData[0]);
+
+for(int i = 0;  i < sequenceData.Count; i++)
+{
+    extrapolatedValues[i] = SequenceAnalyser.GetNextValueInSequence(sequenceData[i]);
+    Console.WriteLine($"Next value for sequence {i + 1}: {extrapolatedValues[i]}");
+}
+
+Console.WriteLine($"Sum of predicted values: {extrapolatedValues.Sum()}");
---
+++ Day9/SequenceAnalyser/SequenceAnalyser.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day9.SequenceAnalyser
+{
+    public static class SequenceAnalyser
+    {
+        public static int GetNextValueInSequence(int[] sequence)
+        {
+            // Getting the initial list and initial last value
+            List<int> NextSequenceToInspect = sequence.ToList();
+            int lastNumberInOriginalSequence = NextSequenceToInspect.Last();
+            int valueToAddForPrediction;
+
+            // Going to add the last number of each list to the last number
+            List<int> lastNumbers = new List<int>();
+            bool allZeros = false;
+
+            while (!allZeros) 
+            {
+                // Initiating a list of differences
+                List<int> listOfDifferences = new List<int>();
+
+                // Creating a list of differences
+                for (int i = 0; i < NextSequenceToInspect.Count - 1; i++)
+                {
+                    listOfDifferences.Add(NextSequenceToInspect[i + 1] - NextSequenceToInspect[i]);
+                }
+
+                // Updating the next list to inspect from the difference list above
+                NextSequenceToInspect = listOfDifferences;
+
+                // Adding the last number of the differences to the list
+                lastNumbers.Add(listOfDifferences.Last());
+
+                // Checking if the list is all zeros
+                allZeros = IsArrayZero(listOfDifferences);
+
+            }
+
+            valueToAddForPrediction = CalculateNextValue(lastNumbers);
+
+            return lastNumberInOriginalSequence + valueToAddForPrediction;
+            
+        }
+
+        private static bool IsArrayZero(List<int> listToInspect)
+        {
+            // Method to check if all array is 0
+            return listToInspect.All(x => x == 0);
+        }
+
+        private static int CalculateNextValue(List<int> listToInspect) 
+        {
+            // Returning the sum of the list
+            return listToInspect.Sum(x => x);
+        }
+    }
+}
