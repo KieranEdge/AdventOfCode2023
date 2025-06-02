aefbad566fbf4698220ba9bd3645795a8b0cf956
Kieran Edge <kesedge94@gmail.com>
2025-06-02 19:39:41 +0100

    Part 1 Complete

---
+++ Day8/DataAccess/DataAccess.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day8.DataAccess
+{
+    public class DataAccess
+    {
+        public Dictionary<string, (string, string)> positionAndDirections = new Dictionary<string, (string, string)>();
+        private string _positionsFilePath;
+        private string _sequenceFilePath;
+        private string _sequenceString;
+        private string[] _allPositionLines;
+        public char[] sequenceCharacters;
+
+        
+        public DataAccess(string positionsFilePath, string sequenceFilePath)
+        {
+            _positionsFilePath = positionsFilePath;
+            _sequenceFilePath = sequenceFilePath;
+            _allPositionLines = File.ReadAllLines(_positionsFilePath);
+            _sequenceString = File.ReadAllText(_sequenceFilePath);
+            DictionaryConstructor();
+            SequenceArrayConstructor();
+        }
+
+        private void DictionaryConstructor()
+        {
+            foreach(string line in _allPositionLines)
+            {
+                // Breaking down the input string into components
+                string position = line.Split(" =")[0];
+                string directionsWithBrackets = line.Split('(')[1];
+                string directionsWithoutBrackets = directionsWithBrackets.Split(')')[0];
+                string leftPosition = directionsWithoutBrackets.Split(", ")[0];
+                string rightPosition = directionsWithoutBrackets.Split(", ")[1];
+
+                positionAndDirections.Add(position, (leftPosition, rightPosition));
+            }
+        }
+
+        private void SequenceArrayConstructor()
+        {
+            List<char> sequence = new List<char>();
+            foreach (char character in _sequenceString)
+            {
+                sequence.Add(character);
+            }
+            sequenceCharacters = sequence.ToArray();
+        }
+    }
+}
---
+++ Day8/Program.cs
@@ -0,0 +1,50 @@
+﻿using Day8.DataAccess;
+
+// Specifying the file path
+string positionFilePath = "C:\\Users\\Kieran Edge\\source\\repos\\AdventOfCode2023\\Day8\\day8_position_data.txt";
+string sequenceFilePath = "C:\\Users\\Kieran Edge\\source\\repos\\AdventOfCode2023\\Day8\\day8_sequence_data.txt";
+
+// Reading the two data files and obtaining a dictionary of positions and an array of sequence characters
+DataAccess dataAccess = new DataAccess(positionFilePath, sequenceFilePath);
+Dictionary<string, (string, string)> positionDirectionDict = dataAccess.positionAndDirections;
+char[] sequenceCharacters = dataAccess.sequenceCharacters;
+
+// Initialising the factors needed for the search
+bool foundZZZ = false;
+int numberOfSteps = 0;
+int sequenceIndex = 0;
+string position = "AAA";
+Console.WriteLine($"Starting Position: {position}");
+
+while (!foundZZZ)
+{
+    /*
+     *  Need to get the next position in the sequence using the index
+     *  Need to get next position using the sequence character
+     *  increment the sequence index then increment then also check if it's out of range and reset it if so
+     *  check if the next position equals ZZZ
+     */
+  
+    char lOrRStep = sequenceCharacters[sequenceIndex];
+    if (lOrRStep == 'L')
+    {
+        position = positionDirectionDict[position].Item1;
+    }
+    else
+    {
+        position = positionDirectionDict[position].Item2;
+    }
+
+    // Logical check to see if the ZZZ position is found
+    foundZZZ = position == "ZZZ";
+
+    // Increment sequence and reseting to zero if at the end
+    sequenceIndex++;
+    if (sequenceIndex >= sequenceCharacters.Length)
+    {
+        sequenceIndex = 0;
+    }
+
+    numberOfSteps++;
+    Console.WriteLine($"Taken {numberOfSteps} steps and moving to {position}, in {lOrRStep} direction");
+}
